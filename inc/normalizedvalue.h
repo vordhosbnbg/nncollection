952fa144d44fce33b3680cfde6f5782ea0bcd15d
Boris Lazarov <vordhosbn.bg@gmail.com>
2018-12-01 19:47:54 +0200

    Added value normalization and better test binaries generation

---
+++ inc/normalizedvalue.h
@@ -0,0 +1,102 @@
+#pragma once
+#include <mutex>
+#include <memory>
+
+template<class T>
+class NormalizedValue
+{
+public:
+
+    NormalizedValue():
+        minVal(0),
+        maxVal(100),
+        minValNormalized(0),
+        maxValNormalized(1),
+        rangeVal(100),
+        reciprocalRangeVal((maxValNormalized - minValNormalized) / (maxVal - minVal)),
+        normalizedValue(0)
+    {
+
+    }
+
+    NormalizedValue(T min, T max, T minNormalized, T maxNormalized) :
+        minVal(min),
+        maxVal(max),
+        minValNormalized(minNormalized),
+        maxValNormalized(maxNormalized),
+        rangeVal(max - min),
+        reciprocalRangeVal((maxValNormalized - minValNormalized) / (maxVal - minVal)),
+        normalizedValue(0)
+    {
+
+    }
+
+    ~NormalizedValue() = default;
+
+    void updateMinMax()
+    {
+        rangeVal= maxVal - minVal;
+        reciprocalRangeVal = (maxValNormalized - minValNormalized) / (maxVal - minVal);
+    }
+
+    T get()
+    {
+        T retVal = static_cast<T>(normalizedValue*rangeVal + minVal);
+        return retVal;
+    }
+
+    void set(T val)
+    {
+        if(val < minVal)
+        {
+            val = minVal;
+        }
+        else if(val > maxVal)
+        {
+            val = maxVal;
+        }
+        normalizedValue = (val - minVal)*reciprocalRangeVal + minValNormalized;
+    }
+
+    double getNormalized()
+    {
+        return normalizedValue;
+    }
+
+    void setNormalized(double val)
+    {
+        normalizedValue = val;
+    }
+
+    T getMin() const
+    {
+        return minVal;
+    }
+
+    void setMin(T val)
+    {
+        minVal = val;
+        updateMinMax();
+    }
+
+    T getMax() const
+    {
+        return maxVal;
+    }
+
+    void setMax(T val)
+    {
+        maxVal = val;
+        updateMinMax();
+    }
+
+
+private:
+    T minVal;
+    T maxVal;
+    T minValNormalized;
+    T maxValNormalized;
+    T rangeVal;
+    T reciprocalRangeVal;
+    double normalizedValue;
+};
---
+++ tst/normalizedvaluetest.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "normalizedvalue.h"
+
+template<typename T>
+void testSpecific(float min, float max, float nMin, float nMax, float test1, float test2, float test3, float test4)
+{
+    std::cout << "Creating NormalizedValue<" << std::string(typeid(T).name()) << "> nv(" << min << "," << max << "," <<
+                 nMin << "," << nMax << ")" << std::endl;
+    NormalizedValue<T> nv(min, max, nMin, nMax);
+    std::cout << "Setting to " << test1 << std::endl;
+    nv.set(test1);
+    std::cout << "Output of getNormalized(): " << std::fixed << nv.getNormalized() << std::endl;
+
+    std::cout << "Setting to " << test2 << std::endl;
+    nv.set(test2);
+    std::cout << "Output of getNormalized(): " << std::fixed << nv.getNormalized() << std::endl;
+
+    std::cout << "Setting to " << test3 << std::endl;
+    nv.set(test3);
+    std::cout << "Output of getNormalized(): " << std::fixed << nv.getNormalized() << std::endl;
+
+    std::cout << "Setting to " << test4 << std::endl;
+    nv.set(test4);
+    std::cout << "Output of getNormalized(): " << std::fixed << nv.getNormalized() << std::endl;
+}
+
+template<typename T>
+void testNormalization()
+{
+    testSpecific<T>(0, 1000, 0, 1, 600, -600, 1600, -1600);
+    testSpecific<T>(-1000, 1000, 0, 1, 600, -600, 1600, -1600);
+    testSpecific<T>(50, 100, -2, 2, 50, 75, -75, 99);
+    testSpecific<T>(0, 1000, -1, 1, 10, 100, 500, 501);
+}
+
+
+int main ([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+{
+    testNormalization<float>();
+    testNormalization<double>();
+    return 0;
+}
